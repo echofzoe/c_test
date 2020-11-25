110ecc32e483c7a68aea2b88713aea47cdacb77c
echofzoe <echofzoe@gmail.com>
2020-11-25 17:41:00 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int climbStairs(int n);
+
+int main() {
+
+    cout << climbStairs(7) << endl;
+
+    return 0;
+}
+
+int climbStairs(int n) {
+    if (n == 0) return 1;
+
+    int twoBack = 1;
+    int oneBack = 1;
+    int cur;
+
+    for (int i = 2; i <= n; ++i) {
+        cur = oneBack + twoBack;
+        twoBack = oneBack;
+        oneBack = cur;
+    }
+
+    return cur;
+}
